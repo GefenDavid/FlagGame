ac72d1d2f80c1a859d855743e0136b281e4da60e
GefenDavid <116173112+GefenDavid@users.noreply.github.com>
2022-10-23 15:44:20 +0300

    Delete mine.py

--- mine.py
+++
@@ -1,7 +0,0 @@
-def locations(game_grid):
-    mine_locations = []
-    for row in range(len(game_grid)):
-        for col in range(len(game_grid[0])):
-            if game_grid[row][col] == 1:
-                mine_locations.append((row, col))
-    return mine_locations
